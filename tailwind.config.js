482207e1be28b4f6248d82325f1df136e5a7a3e6
Robe Santoro <robe.santoro@live.com>
2022-01-18 15:19:07 +0100

    xl fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,8 @@ module.exports = {
       'lg': {'min': '1024px', 'max': '1279px'},
       // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
-      'xl': {'min': '1280px', 'max': '1535px'},
+      'xl': {'min': '1280px', 'max': '2048px'},
       // => @media (min-width: 1280px and max-width: 1535px) { ... }
-
-      '2xl': {'min': '1536px'},
-      // => @media (min-width: 1536px) { ... }
     },
     extend: {},
   },
